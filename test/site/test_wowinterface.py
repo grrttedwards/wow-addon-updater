be38cce988cb8a0fc57be358b65d02b7f5c291c6
Garrett Edwards <grrttedwards@users.noreply.github.com>
2019-08-13 18:37:28 -0400

    Fix wowinterface addon name and add site tests

---
+++ test/site/test_wowinterface.py
@@ -0,0 +1,34 @@
+import unittest
+
+from updater.site import WoWInterface
+
+
+class TestWowInterface(unittest.TestCase):
+    def setUp(self):
+        self.url = 'https://www.wowinterface.com/downloads/info11190-Bartender4.html'
+        self.wowinterface = WoWInterface.WoWInterface(self.url)
+
+    def test_integration_wowinterface_find_zip_url(self):
+        zip_url = self.wowinterface.find_zip_url()
+        # example: https://cdn.wowinterface.com/downloads/file11190/Bartender4-4.8.3.zip?156152944115
+        self.assertRegex(zip_url,
+                         r"https:\/\/cdn.wowinterface.com\/downloads\/file[0-9]+"
+                         r"\/[a-zA-Z0-9]+-[0-9]+\.[0-9]+\.[0-9]+\.zip\?[0-9]+")
+
+    def test_integration_wowinterface_get_addon_name(self):
+        addon_name = self.wowinterface.get_addon_name()
+        self.assertEqual(addon_name, 'Bartender4')
+
+    def test_integration_wowinterface_get_latest_version(self):
+        latest_version = self.wowinterface.get_latest_version()
+        # something like 4.5.6
+        self.assertRegex(latest_version, r"[0-9]+\.[0-9]+\.[0-9]+")
+
+    def test_wowinterface_get_supported_urls(self):
+        supported_urls = self.wowinterface.get_supported_urls()
+        self.assertIsNotNone(supported_urls)
+        self.assertTrue(len(supported_urls) != 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
